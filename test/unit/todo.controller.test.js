1da57a54f95c645ea86df8fcb6e381cc00707d10
Artur Todeschni Crestani <artodeschini@gmail.com>
2024-10-12 00:10:11 -0300

    quando tento chamar getTodos deve retornar um erro

--- test/unit/todo.controller.test.js
+++ test/unit/todo.controller.test.js
@@ -68,6 +68,14 @@ describe("TodoController.createTodo", () => {
             expect(res._getJSONData()).toStrictEqual(allTodos);
         });
 
+        it("should handle errors in getTodos", async () => {
+            const errorMessage = { message: "Error finding" };
+            const rejectedPromise = Promise.reject(errorMessage);
+            TodoModel.find.mockReturnValue(rejectedPromise);
+            await TodoController.getTodos(req, res, next);
+            expect(next).toHaveBeenCalledWith(errorMessage);
+        });
+
     });
 
 
