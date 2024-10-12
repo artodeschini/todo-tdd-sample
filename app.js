56cd55146f216fa807520b2e6e815d8f409c1d04
Artur Todeschni Crestani <artodeschini@gmail.com>
2024-10-11 23:46:56 -0300

    adicionado suporte ao mongodb

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const todoRoutes = require("./routes/todo.routes");
 const app = express();
+const mongodb = require("./mongodb/mongodb.connect");
 
-app.listen(8080, () => {
-    console.log("Server is now running!");
-});
+mongodb.connect();
+
+app.use(express.json());
+app.use("/todos", todoRoutes);
 
 app.get("/", (req, res) => {
     res.json("Hello world!");
